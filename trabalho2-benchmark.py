e397b7490d294e59cb7d4b855c03d604c65a65d1
Matheus Vargas <matheusvargas412@gmail.com>
2021-10-09 19:28:20 -0300

    benchmark adicionado

---
+++ trabalho2-benchmark.py
@@ -0,0 +1,38 @@
+import sys, subprocess, random
+from statistics import mean
+
+if len(sys.argv) < 3:
+    print(f'Digite: python3 {sys.argv[0]} <numero de linhas> <tamanho do bloco>')
+    exit(1)
+
+numero_linhas = int(sys.argv[1])
+tamanho_bloco = int(sys.argv[2])
+qtd_numeros = numero_linhas * tamanho_bloco
+
+with open('entrada.txt', 'w') as file:
+    file.write(str(qtd_numeros) + '\n')
+    for i in range(numero_linhas):
+        for j in range(tamanho_bloco):
+            file.write(str(random.randint(0, 99)) + ' ')
+        file.write('\n')
+
+nthreads = [1, 2, 4]
+num_execucoes = 5
+
+mediaSeq = 0
+
+for n in nthreads:
+    resultados = []
+    print(f'{n} threads:')
+    for i in range(num_execucoes):
+        tempo = float(subprocess.run(f'./a {tamanho_bloco} {n}', shell=True, check=True, capture_output=True, text=True).stdout[:-1])
+        resultados.append(tempo)
+    
+    media = mean(resultados)
+
+    if n == 1:
+        mediaSeq = media
+        print(f'Tempo médio: {media:.6f}s | Aceleração: -\n')
+    
+    else:
+        print(f'Tempo médio: {media:.6f}s | Aceleração: {mediaSeq/media:.6f}\n')
